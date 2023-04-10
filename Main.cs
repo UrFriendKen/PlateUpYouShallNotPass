b428745c54825c8634c5d5cb20ad0ac9e9d0e96b
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-04-10 11:48:59 +0800

    Init

---
+++ Main.cs
@@ -0,0 +1,117 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib;
+using KitchenLib.Event;
+using KitchenLib.References;
+using KitchenMods;
+using System.Collections.Generic;
+using System.Reflection;
+using Unity.Entities;
+using UnityEngine;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenYouShallNotPass
+{
+    public class Main : BaseMod, IModSystem
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "IcedMilo.PlateUp.YouShallNotPass";
+        public const string MOD_NAME = "You Shall Not Pass";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "IcedMilo";
+        public const string MOD_GAMEVERSION = ">=1.1.5";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+
+        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
+#if DEBUG
+        public const bool DEBUG_MODE = true;
+#else
+        public const bool DEBUG_MODE = false;
+#endif
+
+        static HashSet<int> allowedFloorOccupants = new HashSet<int>()
+        {
+            ApplianceReferences.MopWater,
+            ApplianceReferences.MopWaterLong,
+            ApplianceReferences.BuffedFloor,
+            ApplianceReferences.MessCustomer1,
+            ApplianceReferences.MessCustomer2,
+            ApplianceReferences.MessCustomer3,
+            ApplianceReferences.MessKitchen1,
+            ApplianceReferences.MessKitchen2,
+            ApplianceReferences.MessKitchen3
+        };
+
+        // public static AssetBundle Bundle;
+
+        static new Main instance;
+
+        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        protected override void OnInitialise()
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+            instance = this;
+        }
+
+        internal static Entity GetOccupantWithFallbackAndExceptions(Vector3 position, OccupancyLayer layer = OccupancyLayer.Default)
+        {
+            if (Session.CurrentGameNetworkMode != GameNetworkMode.Host)
+                return default;
+
+            Entity occupant = instance.GetOccupant(position, layer);
+            if (occupant == default)
+            {
+                occupant = instance.GetOccupant(position, OccupancyLayer.Floor);
+                if (instance.Require(occupant, out CAppliance appliance) && allowedFloorOccupants.Contains(appliance.ID))
+                {
+                    occupant = default;
+                }
+            }
+            return occupant;
+        }
+
+        //private void AddGameData()
+        //{
+        //    LogInfo("Attempting to register game data...");
+
+        //    // AddGameDataObject<MyCustomGDO>();
+
+        //    LogInfo("Done loading game data.");
+        //}
+
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnPostActivate(KitchenMods.Mod mod)
+        {
+            // TODO: Uncomment the following if you have an asset bundle.
+            // TODO: Also, make sure to set EnableAssetBundleDeploy to 'true' in your ModName.csproj
+
+            // LogInfo("Attempting to load asset bundle...");
+            // Bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).First();
+            // LogInfo("Done loading asset bundle.");
+
+            // Register custom GDOs
+            // AddGameData();
+
+            // Perform actions when game data is built
+            Events.BuildGameDataEvent += delegate (object s, BuildGameDataEventArgs args)
+            {
+            };
+        }
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
---
+++ Patches/MoveMobileAppliances_Patch.cs
@@ -0,0 +1,152 @@
+﻿using HarmonyLib;
+using Kitchen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace KitchenYouShallNotPass.Patches
+{
+    [HarmonyPatch]
+    static class MoveMobileAppliances_Patch
+    {
+        static readonly List<OpCode> OPCODES_TO_MATCH = new List<OpCode>()
+        {
+            OpCodes.Ldarg_0,
+            OpCodes.Ldfld,
+            OpCodes.Ldloc_S,
+            OpCodes.Ldc_I4_0,
+            OpCodes.Call,
+            OpCodes.Stloc_S
+        };
+
+        static readonly List<object> OPERANDS_TO_MATCH = new List<object>()
+        {
+            null,
+            null,
+            null,
+            null,
+            typeof(GenericSystemBase).GetMethod("GetOccupant", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(UnityEngine.Vector3), typeof(KitchenData.OccupancyLayer) }, null),
+            null
+        };
+
+        static readonly List<OpCode> MODIFIED_OPCODES = new List<OpCode>()
+        {
+            //OpCodes.Ldloc_S,
+            //OpCodes.Ldfld,
+            //OpCodes.Ldc_I4,
+            //OpCodes.Ceq,
+            //OpCodes.Br_S
+        };
+
+        static readonly List<object> MODIFIED_OPERANDS = new List<object>()
+        {
+            null,
+            null,
+            null,
+            null,
+            typeof(Main).GetMethod("GetOccupantWithFallbackAndExceptions", BindingFlags.NonPublic | BindingFlags.Static),
+            null
+        };
+
+        const int EXPECTED_MATCH_COUNT = 1;
+
+        public static MethodBase TargetMethod()
+        {
+            Type type = AccessTools.FirstInner(typeof(MoveMobileAppliances), t => t.Name.Contains("c__DisplayClass_OnUpdate_LambdaJob0"));
+            return AccessTools.FirstMethod(type, method => method.Name.Contains("OriginalLambdaBody"));
+        }
+
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> OriginalLambdaBody_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            Main.LogInfo("MoveMobileAppliances Transpiler");
+            Main.LogInfo("Attempt change GetOccupant for target to GetOccupantWithFloorFallback");
+            List<CodeInstruction> list = instructions.ToList();
+
+            int matches = 0;
+            int windowSize = OPCODES_TO_MATCH.Count;
+            for (int i = 0; i < list.Count - windowSize; i++)
+            {
+                for (int j = 0; j < windowSize; j++)
+                {
+                    if (OPCODES_TO_MATCH[j] == null)
+                    {
+                        Main.LogError("OPCODES_TO_MATCH cannot contain null!");
+                        return instructions;
+                    }
+
+                    string logLine = $"{j}:\t{OPCODES_TO_MATCH[j]}";
+
+                    int index = i + j;
+                    OpCode opCode = list[index].opcode;
+                    if (j < OPCODES_TO_MATCH.Count && opCode != OPCODES_TO_MATCH[j])
+                    {
+                        if (j > 0)
+                        {
+                            logLine += $" != {opCode}";
+                            Main.LogInfo($"{logLine}\tFAIL");
+                        }
+                        break;
+                    }
+                    logLine += $" == {opCode}";
+
+                    if (j == 0)
+                        Debug.Log("-------------------------");
+
+                    if (j < OPERANDS_TO_MATCH.Count && OPERANDS_TO_MATCH[j] != null)
+                    {
+                        logLine += $"\t{OPERANDS_TO_MATCH[j]}";
+                        object operand = list[index].operand;
+                        if (OPERANDS_TO_MATCH[j] != operand)
+                        {
+                            logLine += $" != {operand}";
+                            Main.LogInfo($"{logLine}\tFAIL");
+                            break;
+                        }
+                        logLine += $" == {operand}";
+                    }
+                    Main.LogInfo($"{logLine}\tPASS");
+
+                    if (j == OPCODES_TO_MATCH.Count - 1)
+                    {
+                        Main.LogInfo($"Found match {++matches}");
+                        if (matches > EXPECTED_MATCH_COUNT)
+                        {
+                            Main.LogError("Number of matches found exceeded EXPECTED_MATCH_COUNT! Returning original IL.");
+                            return instructions;
+                        }
+
+                        // Perform replacements
+                        for (int k = 0; k < MODIFIED_OPCODES.Count; k++)
+                        {
+                            if (MODIFIED_OPCODES[k] != null)
+                            {
+                                int replacementIndex = i + k;
+                                OpCode beforeChange = list[replacementIndex].opcode;
+                                list[replacementIndex].opcode = MODIFIED_OPCODES[k];
+                                Main.LogInfo($"Line {replacementIndex}: Replaced Opcode ({beforeChange} ==> {MODIFIED_OPCODES[k]})");
+                            }
+                        }
+
+                        for (int k = 0; k < MODIFIED_OPERANDS.Count; k++)
+                        {
+                            if (MODIFIED_OPERANDS[k] != null)
+                            {
+                                int replacementIndex = i + k;
+                                object beforeChange = list[replacementIndex].operand;
+                                list[replacementIndex].operand = MODIFIED_OPERANDS[k];
+                                Main.LogInfo($"Line {replacementIndex}: Replaced operand ({beforeChange ?? "null"} ==> {MODIFIED_OPERANDS[k] ?? "null"})");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Main.LogWarning($"{(matches > 0 ? (matches == EXPECTED_MATCH_COUNT ? "Transpiler Patch succeeded with no errors" : $"Completed with {matches}/{EXPECTED_MATCH_COUNT} found.") : "Failed to find match")}");
+            return list.AsEnumerable();
+        }
+    }
+}
